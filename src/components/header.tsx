900895a95de4d76aed968f0442417bf14b3742f4
manggalang <galangpastibisa@gmail.com>
2024-03-01 15:40:38 +0800

    style: create header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,25 +10,34 @@ const navigation = [
 ];
 
 export default function Header() {
+  const navigationRef = useRef<HTMLFormElement | null>(null);
+  const [openNav, setOpenNav] = React.useState(false);
+  const handleOpen = () => setOpenNav(!openNav);
+
   return (
-    <header className="backdrop-blur	 bg-[rgb(27_31_36_/_0.75)] fixed left-0 top-0 w-full z-50">
+    <header className="backdrop-blur	 bg-[rgb(27_31_36_/_0.75)] fixed left-0 py-[16px] top-0 w-full z-50">
       <div className="bg-transparent container flex items-center justify-between py-[12px]">
         <Link href="/">
           <Image
-            src="/logo.png"
+            src={`/logo.png`}
             alt=""
             height={58}
             width={58}
-            className="h-[58px] w-auto"
+            className="h-[58px] relative w-auto z-[51]"
           />
         </Link>
 
-        <nav className="flex gap-[30px] items-center">
+        <nav
+          ref={navigationRef}
+          className={`bg-dark80 duration-500 fixed flex flex-col gap-[30px] h-screen items-center justify-center left-0 top-0 -translate-y-[1280px] transition w-screen md:bg-transparent md:flex-row md:h-auto md:justify-start md:static md:translate-y-0 md:w-auto ${
+            openNav ? "!translate-y-0" : ""
+          }`}
+        >
           {navigation.map((item) => (
             <Link
               key={item.name}
               href={item.link}
-              className="duration-[300ms] font-semibold text-white transition hover:drop-shadow-[0px_0px_12px_#075FE4]"
+              className="duration-[300ms] font-semibold text-white text-[22px] transition hover:drop-shadow-[0px_0px_12px_#075FE4] md:text-[16px]"
             >
               {item.name}
             </Link>
@@ -34,11 +45,41 @@ export default function Header() {
 
           <Link
             href="/contact"
-            className="bg-primary font-semibold py-[14px] px-[22px] rounded-[10px] text-white"
+            className="bg-primary font-semibold py-[14px] px-[22px] rounded-[10px] text-white text-[22px] md:text-[16px]"
           >
             Contact Me
           </Link>
         </nav>
+
+        <div className="block md:hidden">
+          <button className="relative group" onClick={handleOpen}>
+            <div
+              className={`relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-dark80 ring-0 ring-gray-300 hover:ring-8 ring-opacity-30 duration-200 shadow-md ${
+                !openNav ? "" : "ring-4"
+              }`}
+            >
+              <div
+                className={`flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center ${
+                  !openNav ? "" : "-rotate-[45deg]"
+                }`}
+              >
+                <div
+                  className={`bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 delay-75 ${
+                    !openNav
+                      ? ""
+                      : "h-[1px] origin-right -translate-y-[1px] -rotate-90"
+                  }`}
+                ></div>
+                <div className="bg-white h-[1px] rounded"></div>
+                <div
+                  className={`bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 origin-left delay-75 ${
+                    !openNav ? "" : "h-[1px] -rotate-90 translate-y-[1px]"
+                  }`}
+                ></div>
+              </div>
+            </div>
+          </button>
+        </div>
       </div>
     </header>
   );
