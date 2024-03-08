c9e8def6318cd5fec5aa9593f92a78b8a781fb13
manggalang <galangpastibisa@gmail.com>
2024-03-08 10:21:27 +0800

    feature: install and add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 import "@fontsource-variable/space-grotesk";
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>
         <Header />
         {children}
